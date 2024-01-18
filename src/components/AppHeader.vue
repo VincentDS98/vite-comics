54d490869e329cc2d3bf52c7f5fa5d2fc4078a11
VincentDS98 <disanto.vincenzo98@gmail.com>
2024-01-18 20:17:19 +0100

    first commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,98 @@
+<script>
+export default {
+    data() {
+        return {
+
+        };
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<template>
+    <header>
+        <div class="header-top">
+            <div>
+                <img id="logo" src="../../public/img/dc-logo.png" alt="">
+            </div>
+            <ul>
+                <li>Characters</li>
+                <li>Movies</li>
+                <li>Comics</li>
+                <li>Tv</li>
+                <li>Games</li>
+                <li>Collectibles</li>
+                <li>Video</li>
+                <li>Fans</li>
+                <li>News</li>
+                <li>Shop</li>
+
+            </ul>
+
+        </div>
+
+        <div class="header-bot">
+            <h2>
+            --> Content Goes Here <--
+            </h2>
+        </div>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+.header-top{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+
+    
+}
+
+.header-bot{
+    background-color: black;
+    color: white;
+    padding: 50px;
+
+     h2{
+         margin-left: 270px;
+    }
+   
+
+}
+
+#logo {
+        margin-left: 300px;
+        width:20%;
+    }
+
+ ul{
+        display:flex; 
+        list-style: none;
+        justify-content: space-between;
+        text-decoration: none;
+        font-weight: bold;
+            
+    }
+
+li{
+                margin-right: 100px;
+            
+            }
+html{
+    font-family: Arial, Helvetica, sans-serif;
+}
+
+
+img {
+    width: 100%;
+}
+
+
+
+
+
+
+</style>
